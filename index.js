747909c8785c6127ea1a10078b1faaa6358ceebc
whatrudoing <94329568+whatrudoing@users.noreply.github.com>
2021-12-02 09:24:56 +0900

    메인페이지 애니메이션 움직임 완성

--- index.js
+++ index.js
@@ -1,24 +1,49 @@
-const slides = document.querySelector('.slides');
-const slideImg = document.querySelectorAll('.slides li');
-let currentIdx = 0; 
-const slideCount = slideImg.length; 
-const prev = document.querySelector('.prev');
-const next = document.querySelector('.next'); 
-const slideWidth = 300; 
-const slideMargin = 100; 
-
-slides.style.width = (slideWidth + slideMargin) * slideCount + 'px'; 
-
-function moveSlide(num) { 
-  slides.style.left = -num * 3000 + 'px'; 
-  currentIdx = num; 
+let slides = document.querySelector('.slides');
+let slideImg = document.querySelectorAll('.slides li'); 
+currentIdx = 0; 
+slideCount = slideImg.length; 
+prev = document.querySelector('.prev'); 
+next = document.querySelector('.next'); 
+slideWidth = 1440; 
+slideMargin = 0; 
+makeClone(); 
+initfunction(); 
+function makeClone() { 
+  let cloneSlide_first = slideImg[0].cloneNode(true); 
+  let cloneSlide_last = slides.lastElementChild.cloneNode(true); 
+  slides.append(cloneSlide_first); 
+  slides.insertBefore(cloneSlide_last, slides.firstElementChild); 
 } 
-
-prev.addEventListener('click', function () { 
- if (currentIdx !== 0) moveSlide(currentIdx - 1); 
-}); 
-
+function initfunction() { 
+  slides.style.width = (slideWidth + slideMargin) * (slideCount + 2) + 'px'; 
+  slides.style.left = -(slideWidth + slideMargin) + 'px'; 
+} 
 next.addEventListener('click', function () { 
-  if (currentIdx !== slideCount - 1) { moveSlide(currentIdx + 1); 
-  } 
-});
+   if (currentIdx <= slideCount - 1) { 
+      slides.style.left = -(currentIdx + 2) * (slideWidth + slideMargin) + 'px';
+      slides.style.transition = `${0.5}s ease-out`; 
+    } 
+    if (currentIdx === slideCount - 1) { 
+      setTimeout(function () { 
+       slides.style.left = -(slideWidth + slideMargin) + 'px'; 
+       slides.style.transition = `${0}s ease-out`; 
+      }, 500); 
+       currentIdx = -1; 
+      } 
+      currentIdx += 1; 
+    }); 
+    prev.addEventListener('click', function () { 
+      console.log(currentIdx); 
+      if (currentIdx >= 0) { 
+        slides.style.left = -currentIdx * (slideWidth + slideMargin) + 'px'; 
+        slides.style.transition = `${0.5}s ease-out`; 
+      } 
+      if (currentIdx === 0) { 
+        setTimeout(function () { 
+          slides.style.left = -slideCount * (slideWidth + slideMargin) + 'px'; 
+          slides.style.transition = `${0}s ease-out`; 
+        }, 500); 
+        currentIdx = slideCount; 
+      } 
+      currentIdx -= 1; 
+    });
